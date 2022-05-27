6706eeff38dace9dff132da675750e1494dba491
LeandroAcipreste <logacipreste@gmail.com>
2022-05-26 23:08:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const preencherCard = (endereco) => {
+    document.querySelectorAll('#logradouro').value = endereco.logradouro;
+    window.alert(endereco.logradouro)
+    
+    //document.getElementById('bairro').value = endereco.bairro;
+    //document.getElementById('localidade').value = endereco.localidade;
+    
+    console.log(endereco.logradouro);
+    //console.log(endereco.bairro);
+    //console.log(endereco.localidade)
+    
+}
+
+const pesquisarCep = async() => {
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    preencherCard(endereco);
+}
+ 
+document.getElementById('cep').addEventListener('click', pesquisarCep);
+
+
