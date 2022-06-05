350d40f1c7762ef4bb9593ace7cff4f471535633
LeandroAcipreste <logacipreste@gmail.com>
2022-06-04 23:38:10 -0300

    alterações

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 'use strict';
+// Consultar CEP
+
+const btn = document.getElementById('sendCep')
+
+btn.addEventListener("click", function(e) {
+    
+    e.preventDefault();
+
+    const cep = document.getElementById('cep').value;
+
+    pesquisarCep(cep)
+
+})
+
+
+const pesquisarCep = async(cep) => {
+    
+    const url = `https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep}`;
+    
+    const dados = await fetch(url, {mode: 'cors'});
+    const endereco = await dados.json();
+    preencherCard(endereco);
+
+}
 
 const preencherCard = (endereco) => {
-    document.getElementById('logradouro').value = endereco.logradouro;
-    window.alert(endereco.logradouro)
     
-    //document.getElementById('bairro').value = endereco.bairro;
-    //document.getElementById('localidade').value = endereco.localidade;
+   
+    //document.getElementById('logradouro').innerHTML = "Logradouro: " + endereco.sellers[0];
+    //document.getElementById('bairro').innerText = "Bairro: " + endereco.sellers.name;
+    //document.getElementById('localidade').innerText = "Cidade:  " + endereco.sellers.logo;
     
-    console.log(endereco.logradouro);
+    console.log(endereco);
     //console.log(endereco.bairro);
     //console.log(endereco.localidade)
     
 }
-
-const pesquisarCep = async() => {
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
-    
-    const dados = await fetch(url);
-    const endereco = await dados.json();
-    preencherCard(endereco);
-}
- 
-document.getElementById('cep').addEventListener('click', pesquisarCep);
 
 
