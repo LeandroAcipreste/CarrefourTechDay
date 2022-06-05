55a032199b581a064cc13decfd7a527324efd066
LeandroAcipreste <logacipreste@gmail.com>
2022-06-04 23:40:48 -0300

    alterações

--- script.js
+++ script.js
@@ -4,20 +4,14 @@
 const btn = document.getElementById('sendCep')
 
 btn.addEventListener("click", function(e) {
-    
     e.preventDefault();
-
     const cep = document.getElementById('cep').value;
-
     pesquisarCep(cep)
-
 })
-
 
 const pesquisarCep = async(cep) => {
     
     const url = `https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep}`;
-    
     const dados = await fetch(url, {mode: 'cors'});
     const endereco = await dados.json();
     preencherCard(endereco);
